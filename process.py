e52ba1837bd342be20e5664098b132dd76e4239f
Vicky Qijing Zhang <vickyqijingzhang@gmail.com>
2015-09-23 12:03:05 -0500

    add notebook

--- process.py
+++ process.py
@@ -2,8 +2,14 @@ __author__ = 'vickyzhang'
 
 from pandas import read_csv
 def process():
+    """
+        The main process. It reads in a txt file, assigns values to each word, and sorts it according to the value.
+    """
     words = read_csv('american-words.80', header=None)
     def get_value(word):
+        """
+            A sub-process run on each word. It gets the value of each letter, and add up the values for the whole word.
+        """
         letters = 'abcdefghijklmnopqrstuvwxyz'
         sum = 0
         for letter in word:
@@ -13,10 +19,12 @@ def process():
             sum += letter_value
         return sum
     words['values'] = words[0].apply(get_value)
+    # get those words whose values are 100
     words = words[words['values'] == 100]
+    # get the length of these words and sort ascending
     words['length'] = words[0].apply(len)
     words = words.sort(columns='length')
-    print(words)
+    return words
 
 if __name__ == '__main__':
-    process()
+    print(process())
